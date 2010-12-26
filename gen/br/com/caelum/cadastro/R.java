91f0dfdaa230b00cb6a0fe2ed84b70f214f8cb61
Jonas Alves <jonasfa@gmail.com>
2010-12-26 20:13:18 -0200

    Mostrando mensagem quando não há nenhum evento na lista, formatação

--- gen/br/com/caelum/cadastro/R.java
+++ gen/br/com/caelum/cadastro/R.java
@@ -33,5 +33,6 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int empty_list=0x7f040001;
     }
 }
